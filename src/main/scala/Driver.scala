77cf08f31a5626401f03c894c0bd6e174ad98784
Scott Beamer <sbeamer@lbl.gov>
2016-10-04 13:25:44 -0700

    load in and parse a fir file

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,7 +1,17 @@
 package saga
 
+import scala.io.Source
+
+import firrtl._
+
 object Driver {
   def main(args: Array[String]) = {
-    println("hello!")
+    val argsList = args.toList
+    if (argsList.isEmpty)
+      throw new Exception("Please give input .fir file")
+    val inputFirFile = argsList.head
+    val parsedInput = firrtl.Parser.parse(Source.fromFile(inputFirFile).getLines,
+                                          firrtl.Parser.IgnoreInfo)
+    println(parsedInput)
   }
 }
