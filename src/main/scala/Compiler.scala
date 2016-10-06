61e4a6f62ac50a8119f1234d9b03c7f5d0ae1397
Scott Beamer <sbeamer@lbl.gov>
2016-10-05 18:40:36 -0700

    hackish, but finds write enables of registers

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -1,5 +1,6 @@
 package saga
 
+import collection.mutable.HashMap
 import java.io.Writer
 
 import firrtl._
@@ -7,22 +8,57 @@ import firrtl.Annotations._
 import firrtl.ir._
 import firrtl.Mappers._
 
-
 object DevHelpers {
+  // assumption: registers can only appear in blocks since whens expanded
   def findRegisters(s: Statement): Seq[(String,Type)] = s match {
     case b: Block => b.stmts flatMap findRegisters
     case DefRegister(_, name, tpe, _, _, _) => Seq((name,tpe))
     case _ => Seq()
   }
+
+  val nodeMap = collection.mutable.HashMap[String, Expression]()
+
+  def lastConnected(s: Statement): Statement = {
+    s match {
+      case Connect(_, loc, expr) => loc match {
+        case w: WRef => nodeMap(w.name) = expr
+        case _ =>
+      }
+      case DefNode(_, name, value) => nodeMap(name) = value
+      case _ =>
+    }
+    s map lastConnected
+    s
+  }
+
+  def traceRefs(name: String): Expression = nodeMap(name) match {
+    case w: WRef => traceRefs(w.name)
+    case s => s
+  }
+
+  def identifyWE(e: Expression) = e match {
+    case m: Mux => m.cond match {
+      case w: WRef => w.name
+      case s =>
+    }
+    case e =>
+  }
 }
+
 
 class DevTransform extends Transform {
   def execute(circuit: Circuit, annotationMap: AnnotationMap): TransformResult = {
-    val r = circuit.modules.head match {
-      case m: Module => DevHelpers.findRegisters(m.body)
+    circuit.modules.head match {
+      case m: Module => {
+        val regsAndTypes = DevHelpers.findRegisters(m.body)
+        val regNames = regsAndTypes map (_._1)
+        DevHelpers.lastConnected(m.body)
+        val lastExp = regNames map DevHelpers.traceRefs
+        val writeEnables = lastExp map DevHelpers.identifyWE
+        println(regNames zip writeEnables)
+      }
       case m: ExtModule =>
     }
-    println(r)
     TransformResult(circuit)
   }
 }
