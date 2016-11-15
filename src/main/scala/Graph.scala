52a88dfa0043001400148c510e40ab9e6a0dbdea
Scott Beamer <sbeamer@lbl.gov>
2016-11-15 13:13:37 -0700

    fix topo sort ordering. edges now flow in data direction

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -9,9 +9,9 @@ class Graph {
   val nameToID = HashMap[String,Int]()
   // Numeric vertex ID -> name (string destination variable)
   val idToName = ArrayBuffer[String]()
-  // numeric vertex ID -> list of incoming vertex IDs (consumers)
+  // numeric vertex ID -> list of incoming vertex IDs (dependencies)
   val inNeigh = ArrayBuffer[ArrayBuffer[Int]]()
-  // numeric vertex ID -> list outgoing vertex IDs (dependencies)
+  // numeric vertex ID -> list outgoing vertex IDs (consumers)
   val outNeigh = ArrayBuffer[ArrayBuffer[Int]]()
   // Vertex name -> full C++ string it corresponds to
   val nameToCmd = HashMap[String,String]()
@@ -56,14 +56,14 @@ class Graph {
         throw new Exception("There is a cycle!")
       } else if (!finalMarks(vertexID)) {
         temporaryMarks(vertexID) = true
-        outNeigh(vertexID) foreach { neighborID => visit(neighborID) }
+        inNeigh(vertexID) foreach { neighborID => visit(neighborID) }
         finalMarks(vertexID) = true
         temporaryMarks(vertexID) = false
         finalOrdering += vertexID
       }
     }
     nameToID.values foreach {startingID => visit(startingID)}
-    finalOrdering.reverse
+    finalOrdering
   }
 
   def printCycle(temporaryMarks: ArrayBuffer[Boolean]) {
