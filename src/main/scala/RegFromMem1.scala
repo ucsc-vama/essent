52a8e5a7f1931e7ab03e87ba66c7755e7f0a87a1
Scott Beamer <sbeamer@lbl.gov>
2017-06-28 15:33:18 -0700

    refactor RegFromMem1

--- src/main/scala/RegFromMem1.scala
+++ src/main/scala/RegFromMem1.scala
@@ -12,10 +12,33 @@ import firrtl.Utils._
 object RegFromMem1 extends Pass {
   def name = "Replaces single-element mems with a register"
 
-  def replaceMems(memsToReplace: Set[String])(s: Statement): Statement = {
+  // FUTURE: reduce redundancy with Emitter.grabMemInfo
+  def grabMemConnects(s: Statement): Seq[(String, Expression)] = s match {
+    case b: Block => b.stmts flatMap {s: Statement => grabMemConnects(s)}
+    case c: Connect => { firrtl.Utils.kind(c.loc) match {
+      case firrtl.MemKind => Seq((emitExpr(c.loc), c.expr))
+      case _ => Seq()
+    }}
+    case _ => Seq()
+  }
+
+  // drop mem connects
+  def dropMemConnects(memsToReplace: Set[String])(s: Statement): Statement = {
+    val noConnects = s match {
+      case Connect(_,WSubField(WSubField(WRef(name: String,_,_,_),_,_,_),_,_,_),_) => {
+        if (memsToReplace.contains(name)) EmptyStmt
+        else s
+      }
+      case _ => s
+    }
+    noConnects map dropMemConnects(memsToReplace)
+  }
+
+  // replace mem def's and mem reads
+  def replaceMemsStmt(memsToTypes: Map[String,Type])(s: Statement): Statement = {
     val memsReplaced = s match {
       case mem: DefMemory => {
-        if (memsToReplace.contains(mem.name)) {
+        if (memsToTypes.contains(mem.name)) {
           // FUTURE: should clock be something else?
           // aside: what clock is mem latency relative to?
           DefRegister(mem.info, mem.name, mem.dataType, UIntLiteral(0,IntWidth(1)),
@@ -24,42 +47,10 @@ object RegFromMem1 extends Pass {
       }
       case _ => s
     }
-    memsReplaced map replaceMems(memsToReplace)
+    memsReplaced map replaceMemsStmt(memsToTypes) map replaceMemsExpr(memsToTypes)
   }
 
-  def grabMemConnects(s: Statement): Seq[(String, Expression)] = s match {
-    case b: Block => b.stmts flatMap {s: Statement => grabMemConnects(s)}
-    case c: Connect => {
-      firrtl.Utils.kind(c.loc) match {
-        case firrtl.MemKind => Seq((emitExpr(c.loc), c.expr))
-        case _ => Seq()
-      }
-    }
-    case _ => Seq()
-  }
-
-  // drop mem connects
-  def dropMemConnects(memsToReplace: Set[String])(s: Statement): Statement = {
-    val noConnects = s match {
-      case c: Connect => {
-        c.loc match {
-          case WSubField(WSubField(WRef(name: String,_,_,_),_,_,_),_,_,_) => {
-            if (memsToReplace.contains(name)) EmptyStmt
-            else c
-          }
-          case _ => c
-        }
-      }
-      case _ => s
-    }
-    noConnects map dropMemConnects(memsToReplace)
-  }
-
-  // replace mem reads
-  def replaceMemReadsStmt(memsToTypes: Map[String,Type])(s: Statement): Statement = {
-    s map replaceMemReadsStmt(memsToTypes) map replaceMemReadsExpr(memsToTypes)
-  }
-  def replaceMemReadsExpr(memsToTypes: Map[String,Type])(e: Expression): Expression = {
+  def replaceMemsExpr(memsToTypes: Map[String,Type])(e: Expression): Expression = {
     val replaced = e match {
       case WSubField(WSubField(WRef(name: String, _, _, g: Gender),_,_,_),"data",_,_) => {
         if (memsToTypes.contains(name)) WRef(name, memsToTypes(name), firrtl.RegKind, g)
@@ -67,12 +58,13 @@ object RegFromMem1 extends Pass {
       }
       case _ => e
     }
-    replaced map replaceMemReadsExpr(memsToTypes)
+    replaced map replaceMemsExpr(memsToTypes)
   }
 
   // insert reg write muxes
-  def generateRegUpdates(mems: Seq[DefMemory], memConnects: Map[String, Expression]): Seq[Statement] = {
-    val memsWithWrites = mems filter { !_.writers.isEmpty }
+  def generateRegUpdates(memsToReplace: Seq[DefMemory], body: Statement): Seq[Statement] = {
+    val memConnects = grabMemConnects(body).toMap
+    val memsWithWrites = memsToReplace filter { !_.writers.isEmpty }
     memsWithWrites flatMap { mem => mem.writers map { writePortName => {
       // FUTURE: is this correct gender
       val selfRef = WRef(mem.name, mem.dataType, firrtl.RegKind, firrtl.BIGENDER)
@@ -89,15 +81,16 @@ object RegFromMem1 extends Pass {
     // FUTURE: need to explicitly handle read enables?
     val singleElementMems = allMems filter { _.depth == 1}
     // println(s"${m.name} ${singleElementMems.size}")
-    val memMap = (singleElementMems map { mem => (mem.name, mem)}).toMap
-    val memConnects = grabMemConnects(m.body).toMap
-    val memsReplaced = replaceMems(memMap.keys.toSet)(m.body)
-    val memConnectsRemoved = dropMemConnects(memMap.keys.toSet)(memsReplaced)
-    val memsToTypes = (singleElementMems map { mem => (mem.name, mem.dataType)}).toMap
-    val memReadsReplaced = replaceMemReadsStmt(memsToTypes)(memConnectsRemoved)
-    val regUpdateStmts = generateRegUpdates(singleElementMems, memConnects)
-    val newBlock = Block(Seq(squashEmpty(memReadsReplaced)) ++ regUpdateStmts)
-    Module(m.info, m.name, m.ports, newBlock)
+    if (singleElementMems.isEmpty) m
+    else {
+      val memNamesToReplace = (singleElementMems map { _.name }).toSet
+      val memConnectsRemoved = squashEmpty(dropMemConnects(memNamesToReplace)(m.body))
+      val memsToTypes = (singleElementMems map { mem => (mem.name, mem.dataType)}).toMap
+      val memsReplaced = replaceMemsStmt(memsToTypes)(memConnectsRemoved)
+      val regUpdateStmts = generateRegUpdates(singleElementMems, m.body)
+      val newBlock = Block(Seq(memsReplaced) ++ regUpdateStmts)
+      Module(m.info, m.name, m.ports, newBlock)
+    }
   }
 
   def run(c: Circuit): Circuit = {
