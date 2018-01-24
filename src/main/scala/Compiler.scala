c2192c155a64ad51c7b064c04795d09ce87d05ba
Scott Beamer <sbeamer@lbl.gov>
2018-01-23 17:33:35 -0800

    disable RandInitInvalids pass since firrtl's aggressive DCE obsolesces it

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -705,7 +705,7 @@ class FinalCleanups extends SeqTransform {
     // essent.passes.WireConstProp,
     // essent.passes.ZeroFromBits,
     // essent.passes.WireConstProp,
-    essent.passes.RandInitInvalids,
+    // essent.passes.RandInitInvalids,
     essent.passes.NoResetsOrClockConnects,
     essent.passes.RegFromMem1,
     essent.passes.FactorMemReads)
