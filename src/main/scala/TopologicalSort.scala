1377e707b89fc11380c8cad84d333f04ec09d63b
Scott Beamer <sbeamer@ucsc.edu>
2020-05-17 12:52:19 -0700

    topo sort using new BareGraph api with tests

---
+++ src/main/scala/TopologicalSort.scala
@@ -0,0 +1,67 @@
+package essent
+
+import BareGraph._
+
+import collection.mutable.{ArrayBuffer, BitSet}
+
+object TopologicalSort {
+  def apply(bg: BareGraph) = {
+    val finalOrdering = ArrayBuffer[NodeID]()
+    val inStack = BitSet()
+    val finished = BitSet()
+    def visit(v: NodeID) {
+      if (inStack(v)) {
+        findCyclesByTopoSort(bg) match {
+          case None => throw new Exception("Was a cycle but couldn't reproduce")
+          case Some(cycle) => {
+            cycle foreach println
+            throw new Exception("There is a cycle! (above)")
+          }
+        }
+      } else if (!finished(v)) {
+        inStack.add(v)
+        bg.inNeigh(v) foreach { u => visit(u) }
+        finished.add(v)
+        inStack.remove(v)
+        finalOrdering += v
+      }
+    }
+    bg.nodeRange foreach { startingID => visit(startingID) }
+    finalOrdering
+  }
+
+
+  def findCyclesByTopoSort(bg: BareGraph): Option[Seq[NodeID]] = {
+    var cycleFound: Option[Seq[NodeID]] = None
+    val inStack = BitSet()
+    val finished = BitSet()
+    val callerIDs = ArrayBuffer.fill(bg.numNodes)(-1)
+
+    def backtrackToFindCycle(v: NodeID, cycleSoFar: Seq[NodeID]): Seq[NodeID] = {
+      if (callerIDs(v) == -1) cycleSoFar
+      else if (bg.outNeigh(v).forall(!cycleSoFar.contains(_)))
+        backtrackToFindCycle(callerIDs(v), cycleSoFar ++ Seq(v))
+      else {
+        val loopbackIndices = bg.outNeigh(v) map cycleSoFar.indexOf
+        val trimmedCycle = cycleSoFar.drop(loopbackIndices.max)
+        trimmedCycle ++ Seq(v)
+      }
+    }
+
+    def visit(v: NodeID, callerID: NodeID) {
+      if (inStack(v)) {
+        val cycle = backtrackToFindCycle(callerID, Seq(v))
+        cycleFound = Some(cycle)
+      } else if (!finished(v)) {
+        if (v != callerID)
+          callerIDs(v) = callerID
+        inStack.add(v)
+        bg.inNeigh(v) foreach { u => visit(u, v) }
+        finished.add(v)
+        inStack.remove(v)
+      }
+    }
+    bg.nodeRange foreach { startingID => visit(startingID, startingID) }
+    cycleFound
+  }
+}
---
+++ src/test/scala/TopologicalSortTest.scala
@@ -0,0 +1,27 @@
+package essent
+
+import org.scalatest._
+
+class TopologicalSortSpec extends FlatSpec {
+  "Topological Sort" should "return a valid ordering" in {
+    val bg = new BareGraph
+    bg.addEdge(2,3)
+    bg.addEdge(2,1)
+    bg.addEdge(1,0)
+    bg.addEdge(4,2)
+    val topoSortResult = TopologicalSort(bg)
+    assert(topoSortResult.lastIndexOf(2) < topoSortResult.lastIndexOf(3))
+    assert(topoSortResult.lastIndexOf(2) < topoSortResult.lastIndexOf(1))
+    assert(topoSortResult.lastIndexOf(1) < topoSortResult.lastIndexOf(0))
+    assert(topoSortResult.lastIndexOf(4) < topoSortResult.lastIndexOf(2))
+  }
+
+  it should "detect a cycle" in {
+    val bg = new BareGraph
+    bg.addEdge(2,3)
+    bg.addEdge(3,1)
+    bg.addEdge(3,4)
+    bg.addEdge(3,2)
+    assertThrows[Exception]{ TopologicalSort(bg) }
+  }
+}
