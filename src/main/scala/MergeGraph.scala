85c77788d7ff7eb6035f0d4c3c66a41ecbf28ed9
Scott Beamer <sbeamer@ucsc.edu>
2020-06-25 14:03:29 -0700

    add merging to MergeGraph (w/ tests)

--- src/main/scala/MergeGraph.scala
+++ src/main/scala/MergeGraph.scala
@@ -1,22 +1,25 @@
 package essent
 
+import BareGraph.NodeID
+
 import collection.mutable.{ArrayBuffer, HashMap, HashSet}
 
 
 // Fundamental assumptions:
 //  * IDs match up with IDs in original BareGraph
 //  * The mergeID is a member of the merged group and ID used
+//  * MergeGraph will not add new nodes after built
 
 class MergeGraph extends BareGraph {
   // node ID -> merge ID
-  val idToMergeID = ArrayBuffer[Int]()
+  val idToMergeID = ArrayBuffer[NodeID]()
 
   // merge ID -> [member] node IDs (must include mergeID)
-  val mergeIDToMembers = HashMap[Int, Seq[Int]]()
+  val mergeIDToMembers = HashMap[NodeID, Seq[NodeID]]()
 
   // inherits outNeigh and inNeigh from BareGraph
 
-  def buildFromInitialAssignments(initialAssignments: ArrayBuffer[Int]) {
+  def buildFromInitialAssignments(initialAssignments: ArrayBuffer[NodeID]) {
     // FUTURE: support negative (unassigned) initial assignments
     initialAssignments.copyToBuffer(idToMergeID)
     val asMap = Util.groupIndicesByValue(initialAssignments)
@@ -26,11 +29,19 @@ class MergeGraph extends BareGraph {
       mergeNodesMutably(mergeID, members diff Seq(mergeID))
     }}
   }
+
+  def mergeGroups(mergeDest: NodeID, mergeSources: Seq[NodeID]) {
+    val newMembers = (mergeSources map mergeIDToMembers).flatten
+    newMembers foreach { id => idToMergeID(id) = mergeDest}
+    mergeIDToMembers(mergeDest) ++= newMembers
+    mergeSources foreach { id => mergeIDToMembers.remove(id) }
+    mergeNodesMutably(mergeDest, mergeSources)
+  }
 }
 
 
 object MergeGraph {
-  def apply(og: BareGraph, initialAssignments: ArrayBuffer[Int]) = {
+  def apply(og: BareGraph, initialAssignments: ArrayBuffer[NodeID]) = {
     // FUTURE: cleaner way to do this with clone on superclass?
     val mg = new MergeGraph
     og.outNeigh.copyToBuffer(mg.outNeigh)
--- src/test/scala/MergeGraphTest.scala
+++ src/test/scala/MergeGraphTest.scala
@@ -5,30 +5,102 @@ import collection.mutable.ArrayBuffer
 import org.scalatest._
 
 class MergeGraphSpec extends FlatSpec {
-  "A MergeGraph" should "be built from a BareGraph with initialAssignments" in {
+  val initialAssignments = ArrayBuffer(1,1,1,3,4,6,6)
+
+  def buildStartingMG() = {
     val og = new BareGraph()
     og.addEdge(0,1)
     og.addEdge(0,2)
     og.addEdge(1,2)
-    og.addEdge(4,5)
-    og.addEdge(4,6)
+    og.addEdge(5,6)
     og.addEdge(2,5)
-    val initialAssignments = ArrayBuffer(1,1,1,3,6,6,6)
-    val mg = MergeGraph(og, initialAssignments)
+    og.addEdge(5,3)
+    MergeGraph(og, initialAssignments)
+  }
+
+  "A MergeGraph" should "be built from a BareGraph with initialAssignments" in {
+    val mg = buildStartingMG()
     assert(mg.idToMergeID == initialAssignments)
     assert(mg.mergeIDToMembers == Map(
-      (1,Seq(0,1,2)), (3,Seq(3)), (6,Seq(4,5,6))))
+      (1,Seq(0,1,2)), (3,Seq(3)), (4,Seq(4)), (6,Seq(5,6))))
     assert(mg.outNeigh(0).isEmpty)
     assert(mg.outNeigh(1) == Seq(6))
     assert(mg.outNeigh(2).isEmpty)
     assert(mg.outNeigh(3).isEmpty)
     assert(mg.outNeigh(4).isEmpty)
     assert(mg.outNeigh(5).isEmpty)
-    assert(mg.outNeigh(6).isEmpty)
+    assert(mg.outNeigh(6) == Seq(3))
     assert(mg.inNeigh(0).isEmpty)
     assert(mg.inNeigh(1).isEmpty)
     assert(mg.inNeigh(2).isEmpty)
+    assert(mg.inNeigh(3) == Seq(6))
+    assert(mg.inNeigh(4).isEmpty)
+    assert(mg.inNeigh(5).isEmpty)
+    assert(mg.inNeigh(6) == Seq(1))
+  }
+
+  it should "be able to merge nodes" in {
+    val mg = buildStartingMG()
+    mg.mergeGroups(6, Seq(1))
+    assert(mg.idToMergeID == ArrayBuffer(6,6,6,3,4,6,6))
+    assert(mg.mergeIDToMembers == Map(
+      (3,Seq(3)), (4,Seq(4)), (6,Seq(5,6,0,1,2))))
+    assert(mg.outNeigh(0).isEmpty)
+    assert(mg.outNeigh(1).isEmpty)
+    assert(mg.outNeigh(2).isEmpty)
+    assert(mg.outNeigh(3).isEmpty)
+    assert(mg.outNeigh(4).isEmpty)
+    assert(mg.outNeigh(5).isEmpty)
+    assert(mg.outNeigh(6) == Seq(3))
+    assert(mg.inNeigh(0).isEmpty)
+    assert(mg.inNeigh(1).isEmpty)
+    assert(mg.inNeigh(2).isEmpty)
+    assert(mg.inNeigh(3) == Seq(6))
+    assert(mg.inNeigh(4).isEmpty)
+    assert(mg.inNeigh(5).isEmpty)
+    assert(mg.inNeigh(6).isEmpty)
+  }
+
+  it should "be able to merge nodes #2" in {
+    // Note: merge induces a cycle :(
+    val mg = buildStartingMG()
+    mg.mergeGroups(1, Seq(3))
+    assert(mg.idToMergeID == ArrayBuffer(1,1,1,1,4,6,6))
+    assert(mg.mergeIDToMembers == Map(
+      (1,Seq(0,1,2,3)), (4,Seq(4)), (6,Seq(5,6))))
+    assert(mg.outNeigh(0).isEmpty)
+    assert(mg.outNeigh(1) == Seq(6))
+    assert(mg.outNeigh(2).isEmpty)
+    assert(mg.outNeigh(3).isEmpty)
+    assert(mg.outNeigh(4).isEmpty)
+    assert(mg.outNeigh(5).isEmpty)
+    assert(mg.outNeigh(6) == Seq(1))
+    assert(mg.inNeigh(0).isEmpty)
+    assert(mg.inNeigh(1) == Seq(6))
+    assert(mg.inNeigh(2).isEmpty)
     assert(mg.inNeigh(3).isEmpty)
+    assert(mg.inNeigh(4).isEmpty)
+    assert(mg.inNeigh(5).isEmpty)
+    assert(mg.inNeigh(6) == Seq(1))
+  }
+
+  it should "be able to merge with an empty node" in {
+    val mg = buildStartingMG()
+    mg.mergeGroups(1, Seq(4))
+    assert(mg.idToMergeID == ArrayBuffer(1,1,1,3,1,6,6))
+    assert(mg.mergeIDToMembers == Map(
+      (1,Seq(0,1,2,4)), (3,Seq(3)), (6,Seq(5,6))))
+    assert(mg.outNeigh(0).isEmpty)
+    assert(mg.outNeigh(1) == Seq(6))
+    assert(mg.outNeigh(2).isEmpty)
+    assert(mg.outNeigh(3).isEmpty)
+    assert(mg.outNeigh(4).isEmpty)
+    assert(mg.outNeigh(5).isEmpty)
+    assert(mg.outNeigh(6) == Seq(3))
+    assert(mg.inNeigh(0).isEmpty)
+    assert(mg.inNeigh(1).isEmpty)
+    assert(mg.inNeigh(2).isEmpty)
+    assert(mg.inNeigh(3) == Seq(6))
     assert(mg.inNeigh(4).isEmpty)
     assert(mg.inNeigh(5).isEmpty)
     assert(mg.inNeigh(6) == Seq(1))
