0d86f046a1f54ac1effe092f09b5d4e9ac5990dc
Scott Beamer <sbeamer@ucsc.edu>
2020-06-29 19:09:10 -0700

    initial start to acyclic partitioner refactor/rewrite

---
+++ src/main/scala/AcyclicPart.scala
@@ -0,0 +1,38 @@
+package essent
+
+import collection.mutable.{ArrayBuffer, HashMap, HashSet}
+
+
+// TODO: accept excludeList: HashSet[Int]
+// TODO: take C_p parameter as input
+// TODO: convert back to logging
+
+class AcyclicPart(val mg: MergeGraph) {
+  def coarsenWithMFFCs() {
+    mg.applyInitialAssignments(MFFC(mg))
+  }
+
+  def mergeSingleInputMFFCsToParents(smallZoneCutoff: Int = 20) {
+    val singleInputIDs = mg.nodeRange() filter {
+      id => (mg.inNeigh(id).size == 1) && (mg.nodeSize(id) < smallZoneCutoff)
+    }
+    val singleInputParents = (singleInputIDs flatMap mg.inNeigh).distinct
+    val baseSingleInputIDs = singleInputIDs diff singleInputParents
+    println(s"Merging up ${baseSingleInputIDs.size} single-input zones")
+    baseSingleInputIDs foreach { childID => {
+      val parentID = mg.inNeigh(childID).head
+      mg.mergeGroups(parentID, Seq(childID))
+    }}
+    if (baseSingleInputIDs.size < singleInputParents.size)
+      mergeSingleInputMFFCsToParents(smallZoneCutoff)
+  }
+}
+
+
+object AcyclicPart {
+  def apply(og: BareGraph) = {
+    val ap = new AcyclicPart(MergeGraph(og))
+    // what should return type be? a mergegraph?
+    ap
+  }
+}
---
+++ src/test/scala/AcyclicPartTest.scala
@@ -0,0 +1,62 @@
+package essent
+
+import collection.mutable.ArrayBuffer
+
+import org.scalatest._
+
+
+class AcyclicPartSpec extends FlatSpec {
+  // Test topology, arrows flow left to right
+  // 0 - 1 - 2
+  //       /
+  // 3 - 4 - 5
+  //
+  // 6 - 7
+  def buildStartingBG1() = {
+    val bg = new BareGraph()
+    bg.addEdge(0,1)
+    bg.addEdge(1,2)
+    bg.addEdge(3,4)
+    bg.addEdge(4,5)
+    bg.addEdge(4,2)
+    bg.addEdge(6,7)
+    bg
+  }
+
+  // Test topology, arrows flow left to right
+  // 0 - 1
+  //
+  // 2 - 3
+  //   \
+  //     4
+  // 5
+  //   \
+  // 6 - 7
+  def buildStartingBG2() = {
+    val bg = new BareGraph()
+    bg.addEdge(0,1)
+    bg.addEdge(2,3)
+    bg.addEdge(2,4)
+    bg.addEdge(5,7)
+    bg.addEdge(6,7)
+    bg
+  }
+
+  "An AcyclicPart" should "be built from a BareGraph" in {
+    val ap = AcyclicPart(buildStartingBG1)
+    assertResult(ArrayBuffer(0,1,2,3,4,5,6,7)){ ap.mg.idToMergeID }
+  }
+
+  it should "coarsen by MFFCs" in {
+    val ap = AcyclicPart(buildStartingBG1)
+    ap.coarsenWithMFFCs()
+    assertResult(ArrayBuffer(2,2,2,4,4,5,7,7)){ ap.mg.idToMergeID }
+  }
+
+  // TODO: should actually test smallZoneCutoff argument
+  it should "merge single-input partitions into their parents" in {
+    val ap = AcyclicPart(buildStartingBG2)
+    ap.mergeSingleInputMFFCsToParents()
+    assertResult(ArrayBuffer(0,0,2,2,2,5,6,7)){ ap.mg.idToMergeID }
+  }
+}
