b84045d360b5a1a67e1be5159e1c77321e042f0c
Scott Beamer <sbeamer@ucsc.edu>
2020-07-10 17:52:36 -0700

    add merge small siblings pass to acyclic part

--- src/main/scala/AcyclicPart.scala
+++ src/main/scala/AcyclicPart.scala
@@ -9,6 +9,19 @@ import collection.mutable.{ArrayBuffer, HashSet}
 // TODO: convert back to logging
 
 class AcyclicPart(val mg: MergeGraph, excludeSet: Set[NodeID]) {
+  def perfomMergesIfPossible(mergesToConsider: Seq[Seq[NodeID]]) = {
+    val mergesMade = mergesToConsider flatMap { mergeReq => {
+      assert(mergeReq.size > 1)
+      val partsStillExist = mergeReq.forall{ mg.mergeIDToMembers.contains(_) }
+      if (partsStillExist && mg.mergeIsAcyclic(mergeReq.toSet)) {
+        assert(mergeReq forall { id => !excludeSet.contains(id) })
+        mg.mergeGroups(mergeReq.head, mergeReq.tail)
+        Seq(mergeReq)
+      } else Seq()
+    }}
+    mergesMade
+  }
+
   def coarsenWithMFFCs() {
     val mffcResults = MFFC(mg, excludeSet)
     excludeSet foreach { id => mffcResults(id) = id }
@@ -33,9 +46,29 @@ class AcyclicPart(val mg: MergeGraph, excludeSet: Set[NodeID]) {
       mergeSingleInputPartsIntoParents(smallZoneCutoff)
   }
 
+  def mergeSmallSiblings(smallZoneCutoff: Int = 10) {
+    val smallPartIDs = mg.nodeRange filter {
+      id => (mg.nodeSize(id) < smallZoneCutoff) && (!excludeSet.contains(id))
+    }
+    val inputsAndIDPairs = smallPartIDs map { id => {
+      val inputsCanonicalized = mg.inNeigh(id).toSeq.sorted
+      (inputsCanonicalized, id)
+    }}
+    val inputsToSiblings = Util.groupByFirst(inputsAndIDPairs.toSeq)
+    val mergesToConsider = inputsToSiblings collect {
+      case (inputIDs, siblingIDs) if ((siblingIDs.size > 1) && mg.mergeIsAcyclic(siblingIDs.toSet)) => siblingIDs
+    }
+    println(s"Attempting to merge ${mergesToConsider.size} groups of small siblings")
+    val mergesMade = perfomMergesIfPossible(mergesToConsider.toSeq)
+    if (mergesMade.nonEmpty) {
+      mergeSmallSiblings(smallZoneCutoff)
+    }
+  }
+
   def partition() {
     coarsenWithMFFCs()
     mergeSingleInputPartsIntoParents()
+    mergeSmallSiblings()
     assert(checkPartioning())
   }
 
