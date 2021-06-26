a095cdfb614e4c6c3c497f0043312a7cafaae93b
MSPDUTTA <pdutta1@ucsc.edu>
2021-06-25 19:48:12 -0700

    created test for replacing reserved keywords

--- src/test/scala/ReplaceRsvdKeytest.scala
+++ src/test/scala/ReplaceRsvdKeytest.scala
@@ -1,3 +1,5 @@
+package essent
+import org.scalatest._
 import essent.passes.ReplaceRsvdKeywords
 import firrtl.CircuitState
 import firrtl.options.Dependency
@@ -5,16 +7,25 @@ import firrtl.options.Dependency
 import java.io.{File, FileWriter}
 import scala.io.Source
 
-object ReplaceRsvdKeytest extends App {
-  val sourceReader = Source.fromFile(args(0))
-  val circuit = firrtl.Parser.parse(sourceReader.getLines, firrtl.Parser.IgnoreInfo)
-  sourceReader.close()
+class ReplaceRsvdKeytest extends FlatSpec{
+   "Mypass" should "Replace all reserve keyword" in {
+     val sourceReader = Source.fromURL(getClass.getResource("/Testrsvd.fir"))
+     val circuit = firrtl.Parser.parse(sourceReader.getLines, firrtl.Parser.IgnoreInfo)
+     sourceReader.close()
 
-  val deps = firrtl.stage.Forms.LowFormOptimized ++ Seq(Dependency(ReplaceRsvdKeywords))
-  val firrtlCompiler = new firrtl.stage.transforms.Compiler(deps)
-  val resultState = firrtlCompiler.execute(CircuitState(circuit, Seq()))
-  val debugWriter = new FileWriter(new File(s"${circuit.main}_transformed.fir"))
-  debugWriter.write(resultState.circuit.serialize)
-  debugWriter.close()
+     val deps = firrtl.stage.Forms.LowFormOptimized ++ Seq(Dependency(ReplaceRsvdKeywords))
+     val firrtlCompiler = new firrtl.stage.transforms.Compiler(deps)
+     val resultState = firrtlCompiler.execute(CircuitState(circuit, Seq()))
+     val tmpfile = File.createTempFile(s"${circuit.main}_transformed",".fir")
+     println(tmpfile)
+     val debugWriter = new FileWriter(tmpfile)
+     debugWriter.write(resultState.circuit.serialize)
+     debugWriter.close()
+     val sourceReader1 = Source.fromURL(getClass.getResource("/Testrsvd_correct.fir"))
+     val sourceReader2 = Source.fromFile(tmpfile)
+    // assert(sourceReader1.getLines().sameElements(sourceReader2.getLines()))
+    // tmpfile.delete()
+  }
 }
+
 
